b7fe9b2bb846a662216c3e526c160cb5f51d7661
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-05-31 16:59:54 -0400

    correct test

--- src/DataStructureTest.java
+++ src/DataStructureTest.java
@@ -7,7 +7,7 @@ public class DataStructureTest {
 	@Test
 	public void testGetName() {
 		DataStructure data = new DataStructure("", 0);
-		assertEquals("1", data.getName());
+		assertEquals("", data.getName());
 	}
 
 }
