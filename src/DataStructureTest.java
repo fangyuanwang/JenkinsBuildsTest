3ade4560b2c91f89bb7d7a7e3c50c1ab3d6021b2
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-06-01 08:25:07 -0400

    added one test

--- src/DataStructureTest.java
+++ src/DataStructureTest.java
@@ -9,5 +9,11 @@ public class DataStructureTest {
 		DataStructure data = new DataStructure("", 0);
 		assertEquals("", data.getName());
 	}
+	
+	@Test
+	public void testGetValue() {
+		DataStructure data = new DataStructure("", 0);
+		assertEquals(0, data.getValue());
+	}
 
 }
